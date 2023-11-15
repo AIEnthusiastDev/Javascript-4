a835f040a352281540edc2af00cbc24f4981311c
zejnep1986 <celikzejnep@gmail.com>
2023-11-15 16:41:29 +0100

    new

---
+++ js/function1.js
@@ -0,0 +1,4 @@
+function Run(){
+    let x=document.getElementById("inid").value;
+    document.getElementById("h1id").innerHTML="Hello "+x+"welcome to my website ";
+    }
---
+++ js/function3.js
@@ -0,0 +1,8 @@
+function show(){
+document.getElementById("divid").style.display="block";
+}
+
+function gizle(){
+document.getElementById("divid").style.display="none";
+
+}
